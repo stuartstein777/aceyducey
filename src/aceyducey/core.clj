2611151645c7b75fb0486248c24ef8d7530cef12
Stuart Stein <qmstuart@gmail.com>
2019-04-10 19:35:17 +0100

    AceyDucey finished.

--- src/aceyducey/core.clj
+++ src/aceyducey/core.clj
@@ -1,68 +1,79 @@
 (ns aceyducey.core
   (:gen-class))
 
-; game is:
-; [X] start with deck 1 - 13
-; [X] draw 2 cards.
-; [X] show user these cards.
-;     [X] if 1 show Ace
-;     [X] if 11 show Jack
-;     [X] if 12 show Queen
-;     [X] if 13 show King
-; [X] ask if they want to bet,
-; [X] get their bet,
-; [ ] check bet <= bankroll
-; [ ] if bet ok
-; [ ] draw random card
-; [X] if card between the 2 existing cards.
-; [ ] player wins their bet
-; [ ] increment bankroll
-; [ ] if player loses, decrement their pot.
-; [ ] end when pot reaches 0
-; [ ] if amount bet is 0 print chicken and go again.
-
-; draw a random card from the deck, and return the card and the deck minus that card.
 (defn draw-random-card
   [cards]
-  (let [card (rand-nth cards)
-        index (.indexOf cards card)]
-    {:drawn-card card :remaining-cards (vec (concat (subvec cards 0 index)
-                                                       (subvec cards (inc index))))}))
-
+  (let [card (rand-nth cards)]
+    {:drawn-card (peek cards) :remaining-cards (vec (pop cards))}))
+;
 (defn winning-play?
   [first-card second-card player-card]
   (or (< first-card player-card second-card)
       (> first-card player-card second-card)))
 
+(defn get-bet
+  [bank-roll]
+  (let [bet (-> (read-line)
+                (Integer/parseInt))]
+    (if (> bet bank-roll)
+      (do
+        (println "You can't afford that bet")
+        (recur bank-roll))
+      bet)))
+
+(defn display-card
+  [card]
+  (case card
+    1 "Ace"
+    11 "Jack"
+    12 "Queen"
+    13 "King"
+    (2 3 4 5 6 7 8 9 10) (str card)))
+
 (defn play-game
   [bank-roll]
-  (let [full-deck ["Ace" 2 3 4 5 6 7 8 9 10 "Jack" "Queen" "King"]
-        first-draw (draw-random-card full-deck)
-        first-card (:drawn-card first-draw)
-        second-draw (draw-random-card (:remaining-cards first-draw))
-        second-card (:drawn-card second-draw)
-        remaining-deck (:remaining-cards second-draw)]
-    (println "First card: " first-card)
-    (println "Second card: " second-card)
-    (println "How much do you want to bet [Bankroll: " bank-roll "] ?")
-    (let [bet (read-line)]
-      (when (= 0 bet)
-         (println "Chicken!")
-         (recur bank-roll)))))
+  (if (<= bank-roll 0)
+    (println "Busted! Game over.")
+    (let [full-deck (shuffle [1 2 3 4 5 6 7 8 9 10 11 12 13])
+          first-draw (draw-random-card full-deck)
+          first-card (:drawn-card first-draw)
+          second-draw (draw-random-card (:remaining-cards first-draw))
+          second-card (:drawn-card second-draw)
+          remaining-deck (:remaining-cards second-draw)]
+      (println "First card: " (display-card first-card))
+      (println "Second card: " (display-card second-card))
+      (println "How much do you want to bet [Bankroll: " bank-roll "] ?")
+      (let [bet (get-bet bank-roll)]
+        (if (= 0 bet)
+          (do
+            (println "Chicken!")
+            (flush)
+            (recur bank-roll))
+          (let [player-card (:drawn-card (draw-random-card remaining-deck))]
+            (do
+              (println "Player card: " (display-card player-card))
+              (if (winning-play? first-card second-card player-card)
+                (do
+                  (println "Win!")
+                  (flush)
+                  (recur (+ bank-roll bet)))
+                (do
+                  (println "Lose!")
+                  (flush)
+                  (recur (- bank-roll bet)))))))))))
 
-(defn can-afford-bet
-  [bet bankroll]
-  (<= bet bankroll))
+(defn get-bet
+  [bank-roll]
+  (let [bet (-> (read-line)
+                (Integer/parseInt))]
+    (if (> bet bank-roll)
+      (do
+        (println "You can't afford that bet.")
+        (println "How much do you want to bet [Bankroll: " bank-roll "] ?")
+        (recur bank-roll))
+      bet)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
   (play-game 100))
-
-(let [x 5]
-  (println "x is " 5)
-  (let [y (read-line)]
-    (= x y
-       (println "y is " y)
-       (println "x = y"))))
